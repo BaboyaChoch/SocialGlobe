a50270beeb0202509f5c10566bb3ce1a9815fccf
Baboya Choch <BaboyaChoch@Gmail.com>
2021-09-21 21:37:54 -0500

    fixed map issue

--- screens/Map.js
+++ screens/Map.js
@@ -21,20 +21,18 @@ const styles = StyleSheet.create({
   },
 });
 
-export default function Login() {
+export default function Map() {
   return (
-    <SafeAreaView>
-      <View style={styles.container}>
-        <MapView
-          provider={PROVIDER_GOOGLE} // remove if not using Google Maps
-          style={styles.map}
-          region={{
-            latitude: 37.78825,
-            longitude: -122.4324,
-            latitudeDelta: 0.015,
-            longitudeDelta: 0.0121,
-          }}></MapView>
-      </View>
-    </SafeAreaView>
+    <View style={styles.container}>
+      <MapView
+        provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        style={styles.map}
+        region={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.015,
+          longitudeDelta: 0.0121,
+        }}></MapView>
+    </View>
   );
 }

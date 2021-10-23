3aad64e18e787b6c4b0a53406aec7eb0e932a83f
Brice <your@email.combricetruong67@gmail.com>
2021-10-23 14:17:40 -0500

    update

--- screens/Map.js
+++ screens/Map.js
@@ -7,8 +7,37 @@
  */
 
 import React from 'react';
-import MapView, {PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
-import {StyleSheet, View, SafeAreaView} from 'react-native';
+import MapView, {PROVIDER_GOOGLE, Polyline, Marker} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import MapViewDirections from 'react-native-maps-directions';
+import {StyleSheet, View, Platform, SafeAreaView} from 'react-native';
+import Geolocation from '@react-native-community/geolocation';
+import {requestLocationAccuracy} from 'react-native-permissions';
+import {request, PERMISSIONS} from 'react-native-permissions';
+import {Component} from 'react';
+
+requestLocationPermission = async () => {
+  if (Platform.OS === 'ios') {
+    var response = request(PERMISSIONS.IOS.LOCATION_WHEN_IN_USE);
+    console.log('iPhone: ' + response);
+    if (request === 'granted') {
+      this.locateCurrentPosition();
+    }
+  } else {
+    var response = await request(PERMISSIONS.ANDROID.ACCESS_FINE_LOCATION);
+    console.log('Android: ' + response);
+
+    if (request === 'granted') {
+      this.locateCurrentPosition();
+      this.requestLocationPermission();
+    }
+  }
+};
+
+locateCurrentPosition = () => {
+  Geolocation.getCurrentPosition(position => {
+    console.log(JSON.stringify(position));
+  });
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -21,18 +50,44 @@ const styles = StyleSheet.create({
   },
 });
 
+const GOOGLE_MAPS_APIKEY = ''; // api key = AIzaSyB22w34wSffOSsP9oFAiXl1_-8ryYfZyJc
+const origin = {latitude: 37.78825, longitude: -122.4324};
+const destination = {latitude: 37.78895, longitude: -122.4053769};
+const coordinates = [
+  {latitude: 37.78895, longitude: -122.4053769},
+  {latitude: 37.78795, longitude: -122.4153769},
+];
+
 export default function Map() {
   return (
     <View style={styles.container}>
       <MapView
         provider={PROVIDER_GOOGLE} // remove if not using Google Maps
+        showsUserLocation={true}
         style={styles.map}
         region={{
           latitude: 37.78825,
           longitude: -122.4324,
           latitudeDelta: 0.015,
           longitudeDelta: 0.0121,
-        }}></MapView>
+        }}>
+        <MapViewDirections
+          apikey={GOOGLE_MAPS_APIKEY}
+          origin={coordinates[0]}
+          destination={coordinates[1]}
+          precision={'high'}
+          strokeWidth={4}
+          strokeColor="blue"
+        />
+
+        <Marker coordinate={origin} title={'test'}></Marker>
+        <Polyline
+          coordinates={[
+            {latitude: 37.78895, longitude: -122.4053769},
+            {latitude: 37.78795, longitude: -122.4153769},
+          ]}
+        />
+      </MapView>
     </View>
   );
 }
---
+++ screens/createEventOverlay.js
@@ -0,0 +1,113 @@
+import React, {useState} from 'react';
+import MapView, {Marker, PROVIDER_GOOGLE} from 'react-native-maps'; // remove PROVIDER_GOOGLE import if not using Google Maps
+import {
+  StyleSheet,
+  View,
+  SafeAreaView,
+  Text,
+  ViewBase,
+  TextInput,
+  Button,
+} from 'react-native';
+import {Component} from 'react/cjs/react.production.min';
+import Geolocation from 'react-native-geolocation-service';
+import {Picker} from '@react-native-picker/picker';
+
+const styles = StyleSheet.create({
+  rowStyle: {flexDirection: 'row', marginTop: 10},
+  inputStyle: {
+    height: 50,
+    flex: 1,
+    borderColor: '#5bff33',
+    borderWidth: 1,
+    borderRadius: 20,
+    paddingRight: 50,
+  },
+  textStyle: {
+    color: '#42efdc',
+    fontSize: 25,
+    height: 50,
+    //borderColor: '#5bff33',
+    //borderWidth: 2,
+    //borderRadius: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    paddingLeft: 5,
+    paddingRight: 5,
+  },
+  buttonStyle: {
+    borderRadius: 20,
+    backgroundColor: '#5bff33',
+  },
+});
+
+export default class createEventOverlay extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      fields: ['Address', 'Privacy', 'Description'],
+    };
+  }
+  const [selectedLanguage, setSelectedLanguage] = useState();
+
+  render() {
+    return (
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View style={styles.rowStyle}>
+          <Text style={styles.textStyle}>Title</Text>
+          <TextInput
+            style={styles.inputStyle}
+            placeholder=" Enter Here"></TextInput>
+        </View>
+        <View style={styles.rowStyle}>
+          <Text style={styles.textStyle}>Address</Text>
+          <TextInput
+            style={styles.inputStyle}
+            placeholder=" Enter Here"></TextInput>
+        </View>
+        <View style={styles.rowStyle}>
+          <Text style={styles.textStyle}>Date</Text>
+          <TextInput
+            style={styles.inputStyle}
+            placeholder=" Enter Here"></TextInput>
+        </View>
+        <View style={styles.rowStyle}>
+          <Text style={styles.textStyle}>Time</Text>
+          <TextInput
+            style={styles.inputStyle}
+            placeholder=" Enter Here"></TextInput>
+        </View>
+        <View style={styles.rowStyle}>
+          <Text style={styles.textStyle}>Visibility</Text>
+          <Picker
+            selectedValue={selectedLanguage}
+            onValueChange={(itemValue, itemIndex) =>
+              setSelectedLanguage(itemValue)
+            }>
+            <Picker.Item label="Public" value="Public" />
+            <Picker.Item label="Private" value="Private" />
+          </Picker>
+        </View>
+        /**
+         *if selectedValue == Private
+         *show option to select group  
+         */
+        <View style={{flexDirection: 'column', height: 200, width: '100%'}}>
+          <Text style={styles.textStyle}>Description</Text>
+          <TextInput
+            style={styles.inputStyle}
+            placeholder=" Enter Here"></TextInput>
+        </View>
+        <View style={styles.rowStyle}>
+          <Button title="create" style={styles.buttonStyle}></Button>
+        </View>
+      </View>
+    );
+  }
+}

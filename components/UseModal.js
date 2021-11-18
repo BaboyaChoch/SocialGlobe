769f9b3d53515cf9e60e1d9b211b1fbfbbe25dd0
Brice <your@email.combricetruong67@gmail.com>
2021-11-17 22:39:42 -0600

    save

--- components/UseModal.js
+++ components/UseModal.js
@@ -3,17 +3,17 @@ import {
   StyleSheet,
   View,
   Text,
-  TouchableOpacity,
   Modal,
-  Image,
-  Pressable,
-  ScrollView,
   TouchableWithoutFeedback,
 } from 'react-native';
+import {useNavigation} from '@react-navigation/core';
 import MapView, {Marker} from 'react-native-maps';
+import {IconButton, Colors} from 'react-native-paper';
 
 export default function UseModal(props) {
   const [modalVisible, setModalVisible] = useState(false);
+
+  const navigation = useNavigation();
   return (
     <Marker coordinate={props.coordinate} onPress={() => setModalVisible(true)}>
       <View style={styles.centeredView}>
@@ -25,57 +25,51 @@ export default function UseModal(props) {
           onRequestClose={() => {
             setModalVisible(!modalVisible);
           }}>
-          <TouchableOpacity
-            style={styles.centeredView}
-            activeOpacity={1}
+          <TouchableWithoutFeedback
             onPressOut={() => {
               setModalVisible(!modalVisible);
             }}>
-            <TouchableWithoutFeedback>
-              <View style={styles.centeredView}>
-                <View style={styles.modalView}>
-                  <Text style={styles.modalText}>Title: {props.title}</Text>
-                  <Text style={styles.modalText}>
-                    Description: {props.description}
-                  </Text>
-                  <TouchableOpacity
-                    style={[
-                      styles.button,
-                      styles.modalButtonAlignLeft,
-                      {backgroundColor: 'white'},
-                    ]}
+            <View style={styles.centeredView}>
+              <View style={styles.modalView}>
+                <Text style={styles.modalText}>Title: {props.title}</Text>
+                <Text style={styles.modalText}>
+                  Description: {props.description}
+                </Text>
+                <View
+                  style={{
+                    flexDirection: 'row',
+                    justifyContent: 'center',
+                  }}>
+                  <IconButton
+                    icon="plus"
+                    color={Colors.black}
+                    size={40}
                     onPress={() => {
-                      alert();
-                    }}>
-                    <Image
-                      style={{width: 25, height: 25}}
-                      source={{
-                        uri: 'https://i.ibb.co/60FsSPZ/001-plus.png',
-                      }}
-                    />
-                  </TouchableOpacity>
-                  <Pressable
-                    style={[
-                      styles.button,
-                      styles.modalButtonAlignMiddle,
-                      {backgroundColor: 'black'},
-                    ]}
-                    onPress={() => alert()}>
-                    <Text style={styles.textStyle}>?</Text>
-                  </Pressable>
-                  <Pressable
-                    style={[
-                      styles.button,
-                      styles.modalButtonAlignRight,
-                      {backgroundColor: '#47D13B'},
-                    ]}
-                    onPress={() => alert()}>
-                    <Text style={styles.textStyle}>Go</Text>
-                  </Pressable>
+                      navigation.navigate('Map', {eventToAdd: details});
+                    }}
+                  />
+                  <IconButton
+                    icon="information"
+                    color={Colors.blue300}
+                    size={40}
+                    onPress={() => {
+                      navigation.navigate('EventDetailsPage', {
+                        eventDetails: eventInfo,
+                      });
+                    }}
+                  />
+                  <IconButton
+                    icon="navigation"
+                    color={Colors.green500}
+                    size={40}
+                    onPress={() => {
+                      console.log('reported');
+                    }}
+                  />
                 </View>
               </View>
-            </TouchableWithoutFeedback>
-          </TouchableOpacity>
+            </View>
+          </TouchableWithoutFeedback>
         </Modal>
       </View>
     </Marker>
@@ -92,16 +86,15 @@ const styles = StyleSheet.create({
     margin: 20,
     backgroundColor: 'white',
     borderRadius: 20,
-    padding: 35,
-    alignItems: 'center',
+    padding: 10,
     shadowColor: '#000',
     shadowOffset: {
       width: 0,
       height: 2,
     },
     shadowOpacity: 0.25,
-    shadowRadius: 4,
-    elevation: 5,
+    shadowRadius: 5,
+    elevation: 10,
   },
   button: {
     borderRadius: 20,
@@ -134,5 +127,12 @@ const styles = StyleSheet.create({
     top: '99%',
     alignSelf: 'flex-end',
     marginTop: 25,
+  },
+  tView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'green',
+    marginTop: 22,
   },
 });
